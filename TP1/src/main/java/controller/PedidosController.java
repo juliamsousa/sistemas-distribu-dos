8896a43fe655328513d3b262e8cfc0a374f3e7a6
juliamsousa <juliaeduardasousa@gmail.com>
2022-08-18 11:57:27 -0300

    WIP TP1 - REST

--- TP1/src/main/java/controller/PedidosController.java
+++ TP1/src/main/java/controller/PedidosController.java
@@ -1,11 +1,22 @@
 package controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import services.RegistraEventoService;
+import data.PedidoData;
+
 
 @RestController
+@RequiredArgsConstructor
 public class PedidosController {
-    public ResponseEntity<String> SalvarPedido() {
+    private final RegistraEventoService eventoService;
+
+    @PostMapping(path="/api/salva-pedido")
+    public ResponseEntity<String> SalvarPedido(@RequestBody PedidoData pedido) {
+        eventoService.adicionarEvento("SalvarPedido", pedido);
         return ResponseEntity.ok("Sucesso");
     }
 }
