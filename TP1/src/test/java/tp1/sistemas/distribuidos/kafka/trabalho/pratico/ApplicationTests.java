e662a59ef017555174d3faab540aa6095b2bf089
juliamsousa <juliaeduardasousa@gmail.com>
2022-08-17 23:26:10 -0300

    Criacao inicial do projeto

---
+++ TP1/src/test/java/tp1/sistemas/distribuidos/kafka/trabalho/pratico/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tp1.sistemas.distribuidos.kafka.trabalho.pratico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
