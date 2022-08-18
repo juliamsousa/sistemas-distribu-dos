71600f933ad40abde4c2a3bc5873dd8550f8543c
juliamsousa <juliaeduardasousa@gmail.com>
2022-08-18 12:11:34 -0300

    Refactor

---
+++ TP1-Microservices/src/main/java/tp1/sistemas/distribuidos/kafka/trabalho/pratico/microservico/Application.java
@@ -0,0 +1,15 @@
+package tp1.sistemas.distribuidos.kafka.trabalho.pratico.microservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
